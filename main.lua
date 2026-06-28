b0ef935ff550e677731908b1649467d4040aca93
Ienutjr <160325118+duy5756@users.noreply.github.com>
2026-06-28 18:29:12 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -81,7 +81,7 @@ getgenv().UserConfig = {
         ["Bear"] = {Big = 99, Huge = 99, Rainbow = 99},
         ["Golden Dragonfly"] = {Big = 99, Huge = 99, Rainbow = 99},
         ["Unicorn"] = {Big = 99, Huge = 99, Rainbow = 99},
-        ["Turtle"] = 99,
+        ["Turtle"] = {Big = 99, Huge = 99, Rainbow = 99},
         ["Raccoon"] = 999,
         ["Black Dragon"] = 999,
     },
@@ -131,6 +131,7 @@ getgenv().UserConfig = {
             ["Monkey"] = 1,
             ["Bear"] = 1,
             ["Ice Serpent"] = 1,
+             ["Turtle"] = 1,
         },
         ["Seed"] = {
             ["Rainbow"] = 2,
