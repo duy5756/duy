eb8157bfd1c24c0ded40089c2c81695adbf81d01
Ienutjr <160325118+duy5756@users.noreply.github.com>
2026-07-11 11:52:49 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -34,6 +34,8 @@ getgenv().UserConfig = {
         ["Cherry"] = 2,
         ["Sunflower"] = 2,
         ["Fire Fern"] = 2,
+        ["Poison Apple"] = 2, -- cảm thấy mất tiền thì xoá mấy dòng này vì mua seed tốn tiền lắm
+        ["Venom Spitter"] = 10,
         ["Pomegranate"] = 2, -- cảm thấy mất tiền thì xoá mấy dòng này vì mua seed tốn tiền lắm
         ["Dragon's Breath"] = 100,  
         
