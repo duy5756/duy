09964fc2991871b3669fced7c329508039750ee5
Ienutjr <160325118+duy5756@users.noreply.github.com>
2026-07-13 09:37:58 +0700

    Update accug.lua

--- accug.lua
+++ accug.lua
@@ -62,7 +62,7 @@ getgenv().UserConfig = {
         ["Robin"] = {Big = 99, Huge = 99, Rainbow = 99},
         ["Bee"] = {Big = 99, Huge = 99, Rainbow = 99},
         ["Monkey"] = {Big = 99, Huge = 99, Rainbow = 99},
-        ["Bear"] = 99,
+        ["Bear"] = {Big = 99, Huge = 99, Rainbow = 99},
         ["Golden Dragonfly"] = {Big = 99, Huge = 99, Rainbow = 99},
         ["Unicorn"] = {Big = 99, Huge = 99, Rainbow = 99},
         ["Turtle"] = {Big = 99, Huge = 99, Rainbow = 99},
