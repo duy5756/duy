1ee42105c48c25fd9a23dcb713d6548cc773b619
Ienutjr <160325118+duy5756@users.noreply.github.com>
2026-07-13 18:21:35 +0700

    Update trumf.lua

--- trumf.lua
+++ trumf.lua
@@ -103,8 +103,8 @@ getgenv().UserConfig = {
         },
     },
        -- WH Pet
-    ["Webhook Pet URL"] = "https://discord.com/api/webhooks/1524746579502829690/B9P39iFTqtFJlGGF6Hv8Ipvtlk2yvRc8uVrsZ1jgpstXiwTdnzhVxkPtMPgaYqBeSaNN",
-    ["Webhook Pet Name"] = {"Golden Dragonfly","Unicorn","Raccoon","Monkey","Bee","Ice Serpent","Robin","Deer", "Bunny", "Turtle", "Butterfly", "Bald Eagle"},
+    ["Webhook Pet URL"] = "https://discord.com/api/webhooks/1526186709535162388/FpgsfqoJ6dHL1suopy_dHVK6dR6CJXel-GoBhphqmnUD22Dbrfft-h0_03KluNxp487n",
+    ["Webhook Pet Name"] = {"Golden Dragonfly","Unicorn","Raccoon","Monkey","Bee","Ice Serpent","Robin","Deer", "Bunny", "Turtle", "Butterfly", "Bald Eagle", "Firefly"},
     ["Webhook Pet Rarity"] = {"Mythic", "Super", "Secret"},
  	-- WH Seed
     ["Webhook Seed URL"] = "https://discord.com/api/webhooks/1522633978605998080/vbxArKi1LrVCrX8tCOib7EI3TqjJVqDrtnTOwXNXr1ZFI2dH2VeZvcsD511TfnzZkMIB",
