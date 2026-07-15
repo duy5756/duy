91738e8319e54a3e7a9b8d935d93d4a43798a53e
Ienutjr <160325118+duy5756@users.noreply.github.com>
2026-07-15 22:49:20 +0700

    Update acugtri.lua

--- acugtri.lua
+++ acugtri.lua
@@ -93,7 +93,6 @@ getgenv().UserConfig = {
             "Common Sprinkler",
             "Uncommon Sprinkler",
             "Rare Sprinkler",
-             "Legendary Sprinkler",
         },
     },
        -- WH Pet
@@ -144,7 +143,7 @@ getgenv().UserConfig = {
         ["Gear"] = {
             ["Super Watering Can"] = 1,
             ["Super Sprinkler"] = 1,
-            [ "Basic Pot"] = {Amount = 5, To = "noopbt"},
+              ["Legendary Sprinkler"] = {Amount = 5, To = "noopbt"},
         },
     },
     ["Claim Mail"] = true,
