add473f4e1975d0d959b9f92411f43fb3c623fb8
Ienutjr <160325118+duy5756@users.noreply.github.com>
2026-07-15 22:48:44 +0700

    Update accug.lua

--- accug.lua
+++ accug.lua
@@ -78,8 +78,7 @@ getgenv().UserConfig = {
     ["Auto Collect Seed Packs"] = true,
     ["Gears"] = {
         ["Buy Gear"] = {
-            "Basic Pot",
-              "Common Watering Can",
+            "Common Watering Can",
             "Common Sprinkler",
             "Uncommon Sprinkler",
             "Rare Sprinkler",
@@ -88,11 +87,10 @@ getgenv().UserConfig = {
             "Super Watering Can",
         },
         ["Gears To Use"] = {
-              "Common Watering Can",
+            "Common Watering Can",
             "Common Sprinkler",
             "Uncommon Sprinkler",
             "Rare Sprinkler",
-             "Legendary Sprinkler",
         },
     },
        -- WH Pet
@@ -144,7 +142,7 @@ getgenv().UserConfig = {
         ["Gear"] = {
             ["Super Watering Can"] = 3,
             ["Super Sprinkler"] = 1,
-            ["Basic Pot"] = 4,
+            ["Legendary Sprinkler"] = 5,
         },
     },
     ["Claim Mail"] = true,
