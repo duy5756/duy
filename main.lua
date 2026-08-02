1fa2fafd300d531fa490664619e5dfaf06d3962f
Ienutjr <160325118+duy5756@users.noreply.github.com>
2026-08-02 14:52:58 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -91,16 +91,8 @@ getgenv().UserConfig = {
     ["Unlock Pet Slots"] = 6, -- Số ô chứa pet muốn mở khóa
     ["Auto Collect Seed Packs"] = true, -- Tự động nhặt các túi hạt giống rơi trên mặt đất
     ["Merge Eclipse Bloom"] = true, -- Tự động gộp hoa Eclipse Bloom
-    ["Gears"] = {
+   ["Gears"] = {
         ["Buy Gear"] = {
-		--	["Trowel"] = 500,
-        --    ["Common Watering Can"] = 500,
-        --    ["Common Sprinkler"] = 500,
-        --    ["Uncommon Sprinkler"] = 100,
-        --    ["Rare Sprinkler"] = 100,
-            ["Legendary Sprinkler"] = 100, -- Mua vòi nước Huyền thoại
-            ["Super Sprinkler"] = 50, -- Mua vòi nước Siêu cấp
-            ["Super Watering Can"] = 500, -- Mua bình tưới Siêu cấp
 		   ["Syrup Watering Can"] = 500,
 		   ["Syrup Sprinkler"] = 500,
 			["Harp"] = 100, -- Mua đàn Harp
@@ -112,12 +104,8 @@ getgenv().UserConfig = {
 			["Super Magic Mail"] = 100,
         },
         ["Gears To Use"] = {
-            -- "Trowel", -- Sử dụng xẻng nhỏ
             "Syrup Sprinkler", -- Sử dụng bình tưới nước thường
             "Syrup Watering Can", -- Sử dụng vòi nước thường
-            "Uncommon Sprinkler", -- Sử dụng vòi nước ít phổ biến
-            "Rare Sprinkler", -- Sử dụng vòi nước hiếm
-         --   "Legendary Sprinkler", -- Sử dụng vòi nước huyền thoại
         },
     },
 	-- [ THÔNG BÁO DISCORD - WEBHOOK ]
