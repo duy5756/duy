ebe428902bed193ace3cb10ebea330cb61ff5746
Ienutjr <160325118+duy5756@users.noreply.github.com>
2026-08-04 16:34:02 +0700

    Update accug.lua

--- accug.lua
+++ accug.lua
@@ -100,7 +100,7 @@ getgenv().UserConfig = {
            
         },
         ["Seed"] = {
-		["Maple Bamboo"] = 300,
+		["Maple Bamboo"] = 500,
 		["Maple Venus Fly Trap"] = 1,
 		["Maple Pomegranate"] = 1,
 		["Maple Poison Apple"] = 1,
