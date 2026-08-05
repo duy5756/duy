2ac1dc14e46ab4e7e187ab288fb14d98189506e2
Ienutjr <160325118+duy5756@users.noreply.github.com>
2026-08-05 11:04:22 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -100,7 +100,7 @@ getgenv().UserConfig = {
            
         },
         ["Seed"] = {
-		["Maple Bamboo"] = 500,
+		["Maple Bamboo"] = 800,
 		["Maple Venus Fly Trap"] = 1,
 		["Maple Pomegranate"] = 1,
 		["Maple Poison Apple"] = 1,
