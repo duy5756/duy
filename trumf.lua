55464d824269e7d7846d0c968ddd3e3207e3d7ce
Ienutjr <160325118+duy5756@users.noreply.github.com>
2026-08-05 15:57:36 +0700

    Update trumf.lua

--- trumf.lua
+++ trumf.lua
@@ -38,8 +38,8 @@ getgenv().UserConfig = {
         ["Turkey"] = {Normal = 6 , Big = 99, Huge = 99, Rainbow = 99},
         ["Squirrel"] = {Big = 99, Huge = 99, Rainbow = 99},
         ["Swan"] = {Big = 99, Huge = 99, Rainbow = 99},
-        ["Fox"] = 999,
-        ["Wolf"] = 99,
+        ["Fox"] = {Big = 99, Huge = 99, Rainbow = 99},
+        ["Wolf"] = {Big = 99, Huge = 99, Rainbow = 99},
         ["Shadow Dragon"] = 999,
     },
 	["Equip Pets"] = {
