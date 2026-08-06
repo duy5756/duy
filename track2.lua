331ffcb53cce8071b300e6ad4e256efd187f4992
Ienutjr <160325118+duy5756@users.noreply.github.com>
2026-08-06 14:04:05 +0700

    Update track2.lua

--- track2.lua
+++ track2.lua
@@ -1,1 +1,6 @@
-
+getgenv().Configs = {
+    ['Key'] = "aee2ae7de59508575faef07b40add24f", -- Your key
+    ['Device Name'] = "ug",
+    ['Webhook Notify'] = "", -- For Grow A Garden 2 Only
+}
+loadstring(game:HttpGet("https://nexora-flow.cloud/main.lua"))()
