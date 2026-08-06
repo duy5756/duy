bbb61ac452b0cab7abd09f0fb79f9b51e750af18
Ienutjr <160325118+duy5756@users.noreply.github.com>
2026-08-06 22:14:13 +0700

    Update ben.lua

--- ben.lua
+++ ben.lua
@@ -10,10 +10,6 @@ getgenv().UserConfig = {
         ["Carrot"] = 50, 
             },
     ["Limit Buy Seed"] = {
-		["Maple Carrot"] = 20,
-		["Maple Strawberry"] = 20,
-		["Maple Blueberry"] = 20,
-		["Maple Tulip"] = 20,
 		["Maple Tomato"] = 20,
 		["Maple Apple"] = 20,
 		["Maple Bamboo"] = 2000,
@@ -103,6 +99,7 @@ getgenv().UserConfig = {
            
         },
         ["Seed"] = {
+		["Maple Bamboo"] = 800,
 		["Maple Venus Fly Trap"] = 1,
 		["Maple Pomegranate"] = 1,
 		["Maple Poison Apple"] = 1,
@@ -123,7 +120,7 @@ getgenv().UserConfig = {
 	-- [ CẤU HÌNH NÂNG CAO ]
     ["Auto Plant"] = true, -- Bật tự động trồng cây
     ["Limit Auto Plant"] = 800, -- Giới hạn số lượng trồng tự động tối đa (800)
-    ["Blacklist Seed"] = {"Amber Cranberry" , "Maple Venus Fly Trap" , "Atlantic Giant Pumpkin" , "Maple Pomegranate" , "Maple Poison Apple" , "Maple Venom Spitter"}, -- Danh sách hạt giống bị cấm (Đã fix thêm dấu phẩy ở cuối dòng để tránh lỗi script)
+    ["Blacklist Seed"] = {"Amber Cranberry" , "Maple Venus Fly Trap" , "Atlantic Giant Pumpkin" , "Maple Pomegranate" , "Maple Poison Apple" , "Maple Venom Spitter", "Maple Bamboo"}, -- Danh sách hạt giống bị cấm (Đã fix thêm dấu phẩy ở cuối dòng để tránh lỗi script)
     ["Blacklist Shovel"] = {
         -- Danh sách các loại cây cấm dùng xẻng xúc theo cấp độ
 	"Maple Venus Fly Trap", 
