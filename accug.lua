51519622e19f22c6887e16363966206c299fe791
Ienutjr <160325118+duy5756@users.noreply.github.com>
2026-08-12 09:05:46 +0700

    Update accug.lua

--- accug.lua
+++ accug.lua
@@ -12,12 +12,6 @@ getgenv().UserConfig = {
         ["Carrot"] = 50, 
             },
     ["Limit Buy Seed"] = {
-		["Maple Carrot"] = 20,
-		["Maple Strawberry"] = 20,
-		["Maple Blueberry"] = 20,
-		["Maple Tulip"] = 20,
-		["Maple Tomato"] = 20,
-		["Maple Apple"] = 20,
 		["Maple Bamboo"] = 20,
 		["Maple Corn"] = 20,
 		["Maple Cactus"] = 20,
